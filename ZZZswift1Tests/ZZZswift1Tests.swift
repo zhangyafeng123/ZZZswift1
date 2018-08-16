5126f25fa6720698b40e881017d8868bc59f1739
张亚峰 <zhangyafeng@zhangyafengdemacbook-air.local>
2018-08-16 09:26:19 +0800

    Initial Commit

---
+++ ZZZswift1Tests/ZZZswift1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ZZZswift1Tests.swift
+//  ZZZswift1Tests
+//
+//  Created by 张亚峰 on 2018/8/16.
+//  Copyright © 2018年 zhangyafeng. All rights reserved.
+//
+
+import XCTest
+@testable import ZZZswift1
+
+class ZZZswift1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
